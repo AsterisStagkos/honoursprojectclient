f32e2dadac27d07f9bd05c814bd4116dc97ea77b
Asteris <User@172.20.122.249>
2015-04-02 12:50:49 +0100

    final commit

--- src/com/example/androidhproject/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.androidhproject;
-
-public class Constants {
-
-	static int BTN_STATUS_DOWNLOAD = 1;
-	static int BTN_STATUS_INSTALL = 2;
-	static int BTN_STATUS_PARTICIPATE = 3;
-	static int BTN_STATUS_APPNOTFOUND = 4;
-}
--- src/com/example/androidhproject/ViewHolder.java
+++
@@ -1,8 +0,0 @@
-package com.example.androidhproject;
-
-import android.widget.TextView;
-
-public class ViewHolder {
-	  public static TextView text;
-	  
-	}
